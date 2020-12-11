04cb55f8f3a4124698b87596096e4c71a14c1729
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2020-12-11 09:22:53 -0500

    feat[seq]: added library for tqli function, still demo code

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <math.h>
-#include <stdio.h>
 
-#include "nrutil.h"
+#include <iostream>
+
+using namespace std;
+
+/* ---------------------- Definition ---------------------- */
+#define SIGN(a, b) ((b) >= 0.0 ? fabs(a) : -fabs(a))
 
 /* ---------------------- Prototypes ---------------------- */
 float pythag(float a, float b);
-void tqli(float d[], float e[], int n, float** z);
+void tqli(float* d, float* e, int n, float** z);
 
 /* ---------------------- Main ---------------------- */
 int main(int argc, char** argv) {
@@ -14,15 +18,6 @@ int main(int argc, char** argv) {
 }
 
 /* ---------------------- Functions ---------------------- */
-
-float pythag(float a, float b) {
-    float absa, absb;
-    absa = fabsf(a);
-    absb = fabsf(b);
-    if (absa > absb) return absa * sqrtf(1.0f + (absb / absa) * (absb / absa));
-
-    return (absb == 0.0f ? 0.0f : absb * sqrtf(1.0f + (absa / absb) * (absa / absb)));
-}
 
 /*QL algorithm with implicit shifts, to determine the eigenvalues and eigenvectors of a real,
 symmetric, tridiagonal matrix, or of a real, symmetric matrix previously reduced by tred2 §11.2. On
@@ -33,59 +28,65 @@ may be omitted, as noted in the comments. If the eigenvectors of a tridiagonal m
 the matrix z[1..n][1..n] is input as the identity matrix. If the eigenvectors of a matrix
 that has been reduced by tred2 are required, then z is input as the matrix output by tred2.
 In either case, the kth column of z returns the normalized eigenvector corresponding to d[k].*/
-void tqli(float d[], float e[], int n, float** z) {
-    int m, l, iter, i, k;
+float pythag(float a, float b) {
+    float absa, absb;
+    absa = fabs(a);
+    absb = fabs(b);
+    if (absa > absb)
+        return absa * sqrt(1.0 + (absb / absa) * (absb / absa));
+    else
+        return (absb == 0.0 ? 0.0 : absb * sqrt(1.0 + (absa / absb) * (absa / absb)));
+}
+
+void tqli(float* d, float* e, int n, float** z) {
+    register int m, l, iter, i, k;
     float s, r, p, g, f, dd, c, b;
 
-    for (i = 2; i <= n; i++) e[i - 1] = e[i];
-    // Convenient to renumber the elements of e.
+    for (i = 1; i < n; i++) e[i - 1] = e[i];
     e[n] = 0.0;
-    for (l = 1; l <= n; l++) {
+    for (l = 0; l < n; l++) {
         iter = 0;
         do {
-            // Look for a single small subdiagonal element to split the matrix.
-            for (m = l; m <= n - 1; m++) {
-                dd = fabsf(d[m]) + fabsf(d[m + 1]);
-                if ((float)(fabsf(e[m]) + dd) == dd) break;
+            for (m = l; m < n - 1; m++) {
+                dd = fabs(d[m]) + fabs(d[m + 1]);
+                if ((double)(fabs(e[m]) + dd) == dd) break;
             }
             if (m != l) {
-                if (iter++ == 30) nrerror("Too many iterations in tqli");
-                g = (d[l + 1] - d[l]) / (2.0f * e[l]);  // Form shift.
-                r = pythag(g, 1.0f);
-                g = d[m] - d[l] + e[l] / (g + SIGN(r, g));  // This is dm − ks.
-                s = c = 1.0f;
-                p     = 0.0f;
-
-                for (i = m - 1; i >= l; i--) { /*A plane rotation as in the original
-                        QL, followed by Givens
-                        rotations to restore tridiagonal
-                        form.*/
+                if (iter++ == 30) {
+                    cout << "\n\nToo many iterations in tqli.\n";
+                    exit(1);
+                }
+                g = (d[l + 1] - d[l]) / (2.0 * e[l]);
+                r = pythag(g, 1.0);
+                g = d[m] - d[l] + e[l] / (g + SIGN(r, g));
+                s = c = 1.0;
+                p     = 0.0;
+                for (i = m - 1; i >= l; i--) {
                     f        = s * e[i];
                     b        = c * e[i];
                     e[i + 1] = (r = pythag(f, g));
-                    if (r == 0.0f) {  // Recover from underflow.
+                    if (r == 0.0) {
                         d[i + 1] -= p;
-                        e[m] = 0.0f;
+                        e[m] = 0.0;
                         break;
                     }
                     s        = f / r;
                     c        = g / r;
                     g        = d[i + 1] - p;
-                    r        = (d[i] - g) * s + 2.0f * c * b;
+                    r        = (d[i] - g) * s + 2.0 * c * b;
                     d[i + 1] = g + (p = s * r);
                     g        = c * r - b;
-                    /* Next loop can be omitted if eigenvectors not wanted*/
-                    for (k = 1; k <= n; k++) {  // Form eigenvectors.
+                    for (k = 0; k < n; k++) {
                         f           = z[k][i + 1];
                         z[k][i + 1] = s * z[k][i] + c * f;
                         z[k][i]     = c * z[k][i] - s * f;
-                    }
-                }
-                if (r == 0.0f && i >= l) continue;
+                    } /* end k-loop */
+                }     /* end i-loop */
+                if (r == 0.0 && i >= l) continue;
                 d[l] -= p;
                 e[l] = g;
-                e[m] = 0.0f;
-            }
+                e[m] = 0.0;
+            } /* end if-loop for m != 1 */
         } while (m != l);
     }
 }
