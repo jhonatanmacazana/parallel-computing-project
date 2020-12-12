48ea104a875c5ca17b66347624c801634f32c0a3
EmanuelSamir <samiremp.2@gmail.com>
2020-12-12 18:12:59 -0500

    feat(Diagonalizing): Copied script for symmetric tri-diagonal

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,112 @@ void tqli(float* d, float* e, int n, float** z);
 
 /* ---------------------- Main ---------------------- */
 int main(int argc, char** argv) {
+    // Initialize model: Create tridiagonal matrix 
+    double ** matrix;
+    int dim = 100;
+    
+    // Pass tred2 algorithm. For evaluation, not necessarily
+
+    // Read values diagonal and subdiagonal. Create a identity matrix z 
+    
+    // Apply tqli algorithm
+
+    // Read eigenvalues (d) and eigenvectors (z)
+
+    // Replace values in equation of X(t)
+
     printf("Hello world\n");
     return 0;
 }
 
 /* ---------------------- Functions ---------------------- */
+/*
+*     ** The function
+*     **                tred2()
+*     ** perform a Housholder reduction of a real symmetric matrix
+*     ** a[][]. On output a[][] is replaced by the orthogonal matrix 
+*     ** effecting the transformation. d[] returns the diagonal elements
+*     ** of the tri-diagonal matrix, and e[] the off-diagonal elements, 
+*     ** with e[0] = 0.
+*     ** The function is modified from the version in Numerical recipe.
+*     */
+
+void tred2(float **a, int n, float *d, float *e)
+{
+	   register int    l,k,j,i;
+	   float          scale,hh,h,g,f;
+
+	   for(i = n - 1; i > 0; i--) {
+	       l = i-1;
+	       h = scale= 0.0;
+	           if(l > 0) {
+		            for(k = 0; k <= l; k++)
+		                scale += fabs(a[i][k]);
+		                if(scale == 0.0)               // skip transformation
+		               e[i] = a[i][l];
+			            else {
+	               for(k = 0; k <= l; k++) {
+	               a[i][k] /= scale;          // used scaled a's for transformation
+	                      h       += a[i][k]*a[i][k];
+		                  }
+		            f       = a[i][l];
+	                g       = (f >= 0.0 ? -sqrt(h) : sqrt(h));
+		            e[i]    = scale*g;
+	                h      -= f * g;
+	            a[i][l] = f - g;
+	                f       = 0.0;
+
+	            for(j = 0;j <= l;j++) {
+	                   a[j][i] = a[i][j]/h;       // can be omitted if eigenvector not wanted
+	                  g       = 0.0; 
+	                 for(k = 0; k <= j; k++) {
+	                   g += a[j][k]*a[i][k];
+		                  }
+	             for(k = j+1; k <= l; k++)
+	                  g += a[k][j]*a[i][k];
+		               e[j]=g/h;
+	                      f += e[j]*a[i][j];
+		                  }
+               hh=f/(h+h);
+	            for(j = 0; j <= l;j++) {
+	                   f = a[i][j];
+                 e[j]=g=e[j]-hh*f;
+                 for(k = 0; k <= j; k++)
+	                   a[j][k] -= (f*e[k]+g*a[i][k]);
+		             }
+	             }  // end k-loop
+	          }  // end if-loop for l > 1
+		         else {
+	          e[i]=a[i][l];
+		        }
+		       d[i]=h;
+	          }  // end i-loop
+	    d[0]  = 0.0;
+	    e[0]  = 0.0;
+
+     /* Contents of this loop can be omitted if eigenvectors not
+	 * 	 ** wanted except for statement d[i]=a[i][i];
+	 * 	          */
+          for(i = 0; i < n; i++) {
+	        l = i-1;
+	      if(d[i]) {
+	               for(j = 0; j <= l; j++) {
+	                   g= 0.0;
+	               for(k = 0; k <= l; k++) {
+                      g += a[i][k] * a[k][j];
+	                  }
+                   for (k = 0; k <= l; k++) {
+                  a[k][j] -= g * a[k][i];
+	              }
+            }
+         }
+            d[i]    = a[i][i];
+          a[i][i] = 1.0;
+	        for(j = 0; j <= l; j++)  {
+		         a[j][i]=a[i][j] = 0.0;
+				       }
+			   }
+}
 
 /*QL algorithm with implicit shifts, to determine the eigenvalues and eigenvectors of a real,
 symmetric, tridiagonal matrix, or of a real, symmetric matrix previously reduced by tred2 §11.2. On
